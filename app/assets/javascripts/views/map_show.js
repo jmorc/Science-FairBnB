e8eec80993ac04f0840660796c84e2895744c328
jmorc <jeffcaves@gmail.com>
2014-12-01 09:51:16 -0800

    Re-structured google map logic into MapShow view

--- app/assets/javascripts/views/map_show.js
+++ app/assets/javascripts/views/map_show.js
@@ -1,9 +1,42 @@
 SciFairbnb.Views.MapShow = Backbone.View.extend({
-  template: JST['google_map/show'],
 
+  // initialize google maps and attach to $el
+  mapOptions: {
+            center: new google.maps.LatLng(37.7833, -122.4167),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.NORMAL,
+            panControl: true,
+            scaleControl: false,
+            streetViewControl: true,
+            overviewMapControl: true
+          },
+    
+          addMarker: function(listing){
+            var position = new google.maps.LatLng(listing.get("latitude"), listing.get("longitude"));
+            var marker = new google.maps.Marker({
+              position: position,
+              map: this.map
+            })
+          },
+  
+  initialize: function(){
+  
+this.map = 
+      new google.maps.Map(this.el,this.mapOptions);
+      
+      this.collection.forEach(this.addMarker.bind(this))
+      
+      this.listenTo(this.collection, "add", this.addMarker)
+        
+  },
+  
+  attributes: {
+    style: "height: 100px"
+  },
+  
   render: function(){
-    var content = this.template;
-    this.$el.html(content)
+    // var content = this.template;
+   // this.$el.html(content)
     return this;
   },
 });
